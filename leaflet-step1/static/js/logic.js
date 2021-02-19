34525f9f64b2051a6aac547ce9768fc461de3347
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-02-18 22:22:12 -0800

    working legend and updated css

--- leaflet-step1/static/js/logic.js
+++ leaflet-step1/static/js/logic.js
@@ -32,6 +32,7 @@ function chooseColor (depth) {
     return color;
 };
 
+// geojson layer
 d3.json(queryUrl, function(data) {
     console.log(data.features);
 
@@ -51,5 +52,24 @@ d3.json(queryUrl, function(data) {
         }
     }).addTo(myMap);
 
-
 });
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
